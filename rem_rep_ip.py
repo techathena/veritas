54d4f01db2d6766e818d99664634481d2f64faf8
techathena <41319133+techathena@users.noreply.github.com>
2019-02-01 19:59:13 +0530

    Add files via upload

--- rem_rep_ip.py
+++ rem_rep_ip.py
@@ -3,24 +3,32 @@ from scapy import all
 from scapy.all import *
 
 import getip
+import csv
+import threatcrowdfeed
 
 
-def removerep():
-    makelist = []
+#def removerep():
+makelist = []
     #remove repeating destination ip
-    a = open("filepath.txt","r")
+    #a = open("filepath.txt","r")
     #dta stored in file using test .py  can be accesed using filevar
-    filevar = a.read()
-    a.close()
-    packets = scapy.all.rdpcap(filevar)
+    #filevar = a.read()
+    #a.close()
+packets = scapy.all.rdpcap(r"C:\Users\Jagmohan\Desktop\1.pcapng")
 
-    for p in packets:
+for p in packets:
          ip = getip.ipvarikafun()
          makelist.append(ip)
-    makedict = list(dict.fromkeys(makelist))
-    counted = len(makedict)
-    no = 0
-    for elem in makedict:
-         return(elem)
+
+makedict = list(dict.fromkeys(makelist))
+counted = len(makedict)
+no = 0
+for elem in makedict:
+    with open(r'file1.csv','w') as writefile:
+          writer =csv.writer(writefile)
+          writing = [getip.getipf(),threatcrowdfeed.threatfeed()]
+          writer.writerow(writing)
+
+         #return(elem)
 
 
